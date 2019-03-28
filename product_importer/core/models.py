c4b961a7804ab8348726e69494fec5dfb1f80c03
abhishekram <abhishek8816@gmail.com>
2019-03-28 10:13:06 +0530

    add the product model and its migration

--- product_importer/core/models.py
+++ product_importer/core/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=250)
+    sku = models.CharField(unique=True, max_length=150)
+    description = models.TextField()
+    active = models.BooleanField(default=True)
