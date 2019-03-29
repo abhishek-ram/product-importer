fdbaf0396bb974f117d35c3faccc8c0419a7df34
abhishekram <abhishek8816@gmail.com>
2019-03-29 15:56:51 +0530

    run manage

--- product_importer/settings.py
+++ product_importer/settings.py
@@ -129,7 +129,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "staticfiles")]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Media files i.e. files uploaded by users
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
@@ -146,4 +145,4 @@ CORS_ORIGIN_WHITELIST = (
 )
 
 # Activate Django-Heroku.
-# django_heroku.settings(locals())
+django_heroku.settings(locals(), databases=False)
